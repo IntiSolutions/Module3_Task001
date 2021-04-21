3ac0530f076aa7e14464ef421ea496ea06d44e84
IntiSolutions <80114265+IntiSolutions@users.noreply.github.com>
2021-04-21 16:07:39 +0200

    Add files via upload

---
+++ Task001/index.js
@@ -0,0 +1,22 @@
+console.log("Hello Winc Academy");
+let myName="José Luis Sanchez Mejia";
+console.log(myName);
+let number1=4;
+let number2=4;
+let str1="4";
+let str2="4";
+console.log(number1+number2);
+console.log(str1+str2);
+
+let addition= str1+str2;
+console.log(addition);
+
+let multiplication=number1*number2;
+console.log(multiplication);
+let restwaarde= multiplication%10;
+console.log(restwaarde);
+
+let myAge=37;
+console.log(typeof myAge);
+myAge="37";
+console.log(typeof myAge);
